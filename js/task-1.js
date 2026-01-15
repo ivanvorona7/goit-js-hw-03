cb4669e232a00ea3ced025c254765fe5ea89958e
Ivan Vorona <ivorona07@gmail.com>
2026-01-15 23:22:31 +0200

    Completed the task

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+function slugify(title) {
+  const words = title.toLowerCase().split(" "); 
+  const slugArr = [];
+
+  for (const word of words) {
+    slugArr.push(word); 
+  }
+
+  return slugArr.join("-"); 
+}
+
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
